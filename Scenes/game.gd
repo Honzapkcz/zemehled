2abba8dbbb8abc0c6b7d28ce80ff9788ba8cad2e
Honzapkcz <honzapkc@gmail.com>
2024-03-14 14:57:36 +0100

    Moved everything from game.gd to global.gd

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -1,19 +1,3 @@
 extends Control
 
-class ZemeGame:
-	var game_name: String
-	var description: String
-	var rounds: int
-	var difficulty: int
-	var game_image: String
-	var map_image: String
-	var images: PackedStringArray
-	var positions: PackedVector2Array
-	
-	func _init(path := ""):
-		if path.is_empty():
-			return
-		self.load(path)
-	
-	func load(path):
-		pass
+
