4bbde753cd8d6c6f7152af4002e90534ccd5d9f3
Honzapkcz <honzapkc@gmail.com>
2024-03-15 22:53:22 +0100

    Allow dragging the map

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+var drag_active: bool
+
 func load_map(path: String):
 	$Sprite2D.texture = load(path)
 
 func get_point() -> Vector2:
 	return Vector2.ZERO
+
+func _input(event):
+	if event is InputEventMouseButton:
+		
+		drag_active = event.pressed
+	elif event is InputEventMouseMotion:
+		if not drag_active:
+			return
+		$Sprite2D.position += event.relative
+	
