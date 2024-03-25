c81d33bd5a27a0442203aa7de76c275862fe8c35
Honzapkcz <honzapkc@gmail.com>
2024-03-25 13:37:34 +0100

    Move smoothly when teleported to location

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -23,7 +23,7 @@ func show_location(location: Vector2):
 	$Map/Point2.position = location
 	$Map/Point2.visible = true
 	offset = -location
-	$Map.position = offset * $Map.scale + get_viewport_rect().size / 2
+	get_tree().create_tween().tween_property($Map, "position", offset * $Map.scale + get_viewport_rect().size / 2, 1.0)
 
 func center_map():
 	offset = -$Map/Sprite2D.get_rect().size / 2
