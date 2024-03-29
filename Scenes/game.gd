d3d2c56974111e4e5733d17bffab2537730eb7aa
Honzapkcz <honzapkc@gmail.com>
2024-03-29 22:10:59 +0100

    Reworked score system and added end dialog

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -4,7 +4,7 @@ var popup_open: bool
 var question: int = -1
 var time_left: int
 var playing: bool = true
-var position_delta: PackedVector2Array
+var position_delta: PackedInt32Array
 var submit_clicked: bool
 var order: PackedInt32Array
 
@@ -61,14 +61,16 @@ func mainloop():
 			time_left -= 1
 			if not playing or submit_clicked:
 				break
-		position_delta.append(Global.game.positions[order[question]] - $Margin/Map/View/Map.get_point())
+		position_delta.append(round(sqrt((Global.game.positions[order[question]].round().x
+		- $Margin/Map/View/Map.get_point().round().x)**2 + (Global.game.positions[order[question]].round().y
+		- $Margin/Map/View/Map.get_point().round().y)**2)))
 		$Margin/Map/View/Map.show_location(Global.game.positions[order[question]])
 		$DropEffect.play()
-		$Submit/VBox/Label.text = "Rozdíl: " + str(abs(Global.game.positions[order[question]].round().x
-		- $Margin/Map/View/Map.get_point().round().x) + abs(Global.game.positions[order[question]].round().y
-		- $Margin/Map/View/Map.get_point().round().y))
+		$Submit/VBox/Label.text = "Rozdíl: " + str(position_delta[-1])
+		$Margin/Map/View/Map.can_point = false
 		if playing:
 			await $Submit/VBox/Button.pressed
+		$Margin/Map/View/Map.can_point = true
 		$Margin/Map/View/Map.clear_point()
 		$Submit/VBox/Label.text = ""
 		$MoveEffect.play()
@@ -78,8 +80,22 @@ func mainloop():
 
 func end():
 	popup_open = true
+	$Ending.visible = true
+	$MoveEffect.play()
+	var x: int
+	for i in position_delta:
+		if i > x:
+			x = i
+	$Ending/Borders/Layout/Max.text = str(x)
+	for i in position_delta:
+		x += i
+	$Ending/Borders/Layout/Sum.text = str(x)
+	$Ending/Borders/Layout/Average.text = str(x / position_delta.size())
+	get_tree().create_tween().tween_property($Ending, "position:y", 120, 1.0)
+	await $Ending/Header/Close.pressed
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property($Status, "position:x", -120, 1.0)
+	tween.tween_property($Ending, "position:y", 650, 1.0)
+	tween.parallel().tween_property($Status, "position:x", -120, 1.0)
 	tween.parallel().tween_property($Image, "position:x", get_viewport_rect().size.x, 1.0).set_delay(0.25)
 	tween.parallel().tween_property($Margin, "scale", Vector2(0.0, 0.0), 1.0).set_delay(0.5)
 	tween.parallel().tween_property($Border, "modulate:a", 0.0, 1.0).set_delay(0.75)
